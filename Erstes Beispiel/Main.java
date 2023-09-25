537ec17c15e930f707a627b491bf7da459ad8f06
Danjka99 <jakob_daniel@gmx.net>
2023-09-25 11:29:21 +0200

    sync

---
+++ Erstes Beispiel/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Main wird ausgeführt");
+    
+    for(int i = 0; i < args.length; i++) {
+    	System.out.println(args[i]);
+    }
+  }
+}
